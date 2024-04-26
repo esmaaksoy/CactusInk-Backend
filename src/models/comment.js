25313bf48a93d8e605105b92f6b4557852e7716b
esma <esma.ea.aksoy@gmail.com>
2024-04-26 14:33:12 +0300

    feat: Comment model

---
+++ src/models/comment.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const { mongoose } = require("../configs/dbConnection");
+
+const CommentSchema = new mongoose.Schema(
+  {
+    blogId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Blog",
+      required: [true, "blogId field must be required"],
+    },
+    
+    comment: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+  },
+  {
+    collection: "categories",
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Comment", CommentSchema);
